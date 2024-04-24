94e33273aa411d7e3e4c7d75664f4d04af05d4fc
Binay Paul <binay.paul.1993@gmail.com>
2024-04-24 09:34:53 +0530

    middle of list solved

--- src/main/java/DataStructure/Linear/LinkedList/LinkedList.java
+++ src/main/java/DataStructure/Linear/LinkedList/LinkedList.java
@@ -1,24 +1,33 @@
 package DataStructure.Linear.LinkedList;
 
 public class LinkedList {
-    public Node head = null;
+    private Node head = null;
+    public Node head() {
+        return this.head;
+    }
+
+
     public LinkedList(int... elements) {
+        Node curr = null;
         for (int i = 0; i < elements.length; i++) {
             if(i == 0) {
                 head = new Node(elements[0]);
+                curr = head;
             } else {
                 Node node = new Node(elements[i]);
-                head.next = node;
+                curr.next = node;
+                curr = node;
             }
         }
     }
 
     public static void printList(Node head) {
         Node curr = head;
-        while (curr.next != null) {
-            System.out.print("| " + curr.data + " -> ");
+        while (curr != null) {
+            System.out.print(curr.data + " -> ");
             curr = curr.next;
         }
         System.out.print("NULL");
+        System.out.println();
     }
 }
--- src/main/java/DataStructure/Linear/LinkedList/Problems/MiddleOfALinkedList.java
+++ src/main/java/DataStructure/Linear/LinkedList/Problems/MiddleOfALinkedList.java
@@ -3,10 +3,36 @@ package DataStructure.Linear.LinkedList.Problems;
 import DataStructure.Linear.LinkedList.LinkedList;
 import DataStructure.Linear.LinkedList.Node;
 
-public class MiddleOfALinkedList {
+public class MiddleOfALinkedList extends LinkedList {
     public static void main(String[] args) {
-        Node head = new LinkedList(1,2,3,4,5).head;
-
+        Node head = new LinkedList(1,2,3,4,5,6).head();
         LinkedList.printList(head);
+        MiddleOfALinkedList.findMiddle(head);
+
+        Node head1 = new LinkedList(1,2,3,4,5,6,7).head();
+        LinkedList.printList(head1);
+        MiddleOfALinkedList.findMiddle(head1);
+    }
+
+    public static void findMiddle(Node head) {
+        //base condition
+        if(head.next == null) {
+            System.out.println(head.data);
+        }
+
+        //main condition
+        Node slow = head;
+        Node fast = head;
+        Node prevOfSlow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            prevOfSlow = slow;
+            slow = slow.next;
+        }
+        if(fast == null) {
+            System.out.println(prevOfSlow.data + ", " + slow.data);
+        } else {
+            System.out.println(slow.data);
+        }
     }
 }
