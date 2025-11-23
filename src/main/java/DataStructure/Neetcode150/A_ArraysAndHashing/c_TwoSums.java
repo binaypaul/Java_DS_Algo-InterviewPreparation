4a9fa98fb33e11b02f2fef98877eb477a0954088
Binay Paul <binay.paul.1993@gmail.com>
2025-11-23 12:14:19 +0530

    surrounded regions

--- src/main/java/DataStructure/Neetcode150/A_ArraysAndHashing/c_TwoSums.java
+++ src/main/java/DataStructure/Neetcode150/A_ArraysAndHashing/c_TwoSums.java
@@ -1,20 +1,24 @@
 package DataStructure.Neetcode150.A_ArraysAndHashing;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class c_TwoSums {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(twoSum(new int[]{2,11,7,15}, 9)));
+        System.out.println(Arrays.toString(twoSum(new int[]{3,3,3}, 6)));
     }
 
     public static int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i+1; j < nums.length; j++) {
-                if(nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
+        Map<Integer, Integer> map = new HashMap<>();
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            int rem = target - nums[i];
+            if(map.containsKey(rem)){
+                return new int []{i, map.get(rem)};
             }
+            map.put(nums[i], i);
         }
-        return null;
+        return new int[]{};
     }
 }
