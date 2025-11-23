08f9ba6546ce7044d52177f44e1c45864bb36dab
Binay Paul <binay.paul.1993@gmail.com>
2025-11-23 15:38:25 +0530

    surrounded regions

--- src/main/java/DataStructure/Neetcode150/J_Heap_OR_PriorityQueue/practise/MedianFinder.java
+++ src/main/java/DataStructure/Neetcode150/J_Heap_OR_PriorityQueue/practise/MedianFinder.java
@@ -1,18 +1,12 @@
-package DataStructure.Neetcode150.J_Heap_OR_PriorityQueue;
+package DataStructure.Neetcode150.J_Heap_OR_PriorityQueue.practise;
 
 import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
-//https://leetcode.com/problems/find-median-from-data-stream/description/
-class MedianFinder {
-    private Queue<Integer> minHeap = null;
-    private Queue<Integer> maxHeap = null;
-
-    public MedianFinder() {
-        minHeap = new PriorityQueue<>();
-        maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
-    }
+public class MedianFinder {
+    Queue<Integer> minHeap = new PriorityQueue<>();
+    Queue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
 
     public void addNum(int num) {
         //maxHeap to store smaller half of total elements
@@ -25,26 +19,22 @@ class MedianFinder {
             maxHeap.add(num);
         }
         //rebalancing heap
-        //minHeap can be equal or 1 less than maxHeap
         if (minHeap.size() > maxHeap.size()) {
             maxHeap.add(minHeap.remove());
         }
-        //maxHeap size can be equal or 1 greater than minHeap
         if(maxHeap.size()-minHeap.size() > 1) {
             minHeap.add(maxHeap.remove());
         }
     }
 
     public double findMedian() {
-         if(maxHeap.size() == minHeap.size()) {
-             return (maxHeap.peek()+ minHeap.peek())/2.0;
-         } else {
-             return maxHeap.peek();
-         }
+        if(maxHeap.size() == minHeap.size()) {
+            return ((maxHeap.peek()+ minHeap.peek())/2.0);
+        } else {
+            return maxHeap.peek();
+        }
     }
-}
 
-public class g_FindMedianFromDataStream {
     public static void main(String[] args) {
         MedianFinder mf = new MedianFinder();
         mf.addNum(1);
@@ -57,10 +47,5 @@ public class g_FindMedianFromDataStream {
         System.out.println(mf.findMedian());
         mf.addNum(5);
         System.out.println(mf.findMedian());
-        System.out.println("Hello");
-        System.out.println("Hello");
-        int max = 6;
-        int i = max + 1;
-        double time = 12.44;
     }
 }
