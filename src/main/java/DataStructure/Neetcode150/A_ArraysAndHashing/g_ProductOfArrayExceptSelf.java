83e753773529f709df76772c472fef59f0b53b90
Binay Paul <binay.paul.1993@gmail.com>
2025-11-23 13:03:09 +0530

    surrounded regions

--- src/main/java/DataStructure/Neetcode150/A_ArraysAndHashing/g_ProductOfArrayExceptSelf.java
+++ src/main/java/DataStructure/Neetcode150/A_ArraysAndHashing/g_ProductOfArrayExceptSelf.java
@@ -25,6 +25,8 @@ public class g_ProductOfArrayExceptSelf {
         return res;
         */
 
+        //! prefix prod
+        /*
         int n = nums.length;
         int[] res = new int[n];
         int[] prefixProd = new int[n];
@@ -43,5 +45,30 @@ public class g_ProductOfArrayExceptSelf {
             res[i] = prefixProd[i] * suffixProd[i];
         }
         return res;
+        */
+
+        var totalProd = 1;
+        var zeroIndex = -1;
+        var zeroCount = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i] ==0) {
+                zeroCount++;
+                zeroIndex = i;
+            } else {
+                totalProd *= nums[i];
+            }
+        }
+        if(zeroCount>1) {
+            Arrays.fill(nums, 0);
+        } else if(zeroCount == 1){
+            Arrays.fill(nums, 0);
+            nums[zeroIndex] = totalProd;
+        } else {
+            for (int i = 0; i < nums.length; i++) {
+                int num = nums[i];
+                nums[i] = totalProd/num;
+            }
+        }
+        return nums;
     }
 }
