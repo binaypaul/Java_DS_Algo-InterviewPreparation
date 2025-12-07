354d3b1e01f146f3dd5928c39777eae8a05ada15
Binay Paul <binay.paul.1993@gmail.com>
2025-12-07 10:28:58 +0530

    verbiage change

--- src/main/java/DataStructure/Neetcode150/B_TwoPointers/b_TwoSumII_InputArrayIsSorted.java
+++ src/main/java/DataStructure/Neetcode150/B_TwoPointers/b_TwoSumII_InputArrayIsSorted.java
@@ -19,7 +19,7 @@ import java.util.Arrays;
  */
 public class b_TwoSumII_InputArrayIsSorted {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(twoSum(new int[]{2,7,11,15}, 9)));
+        System.out.println(Arrays.toString(twoSum(new int[]{1,2,3,6,7,11,15}, 9)));
     }
 
     public static int[] twoSum(int[] numbers, int target) {
