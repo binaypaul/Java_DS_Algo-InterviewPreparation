17a0f57f6e44d1ba786be150af0ca746c8dd9962
Binay Paul <binay.paul.1993@gmail.com>
2025-12-27 02:05:20 +0530

    verbiage change

--- src/main/java/DataStructure/Neetcode150/D_BinarySearch/d_d_FindMaximumInRotatedSortedArray.java
+++ src/main/java/DataStructure/Neetcode150/D_BinarySearch/d_d_FindMaximumInRotatedSortedArray.java
@@ -5,9 +5,22 @@ public class d_d_FindMaximumInRotatedSortedArray {
         System.out.println(new d_d_FindMaximumInRotatedSortedArray().findMaxWithDup(new int[] {4,5,6,7,8,9,1,2,3}));
     }
 
-    private int findMaxWithDup(int[] nums) {
-        int max = Integer.MIN_VALUE;
-        int l = 0, r = nums.length-1;
-        return max;
+    public int findMaxWithDup(int[] nums) {
+        int l = 0, r = nums.length - 1;
+        //1,2,3,4,5
+        //3,4,5,1,2
+        //4,5,1,2,3
+        while (l<r) {
+            int mid = l+(r-l)/2;
+            if(nums[mid] < nums[r]) {
+                r = mid;
+            } else if(nums[mid] > nums[r]){
+                l = mid + 1;
+            } else {
+                r--;
+            }
+        }
+        if(l == 0) return nums[nums.length - 1];
+        else return nums[l-1];
     }
 }
