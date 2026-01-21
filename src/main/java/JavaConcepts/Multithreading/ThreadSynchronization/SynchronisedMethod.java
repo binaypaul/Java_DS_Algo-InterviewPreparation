5f1bc4c441e3f351d799a8a84ee3efed083bdb2c
Binay Paul <binay.paul.1993@gmail.com>
2026-01-22 02:31:05 +0530

    verbiage change

--- src/main/java/JavaConcepts/Multithreading/ThreadSynchronization/SynchronisedMethod.java
+++ src/main/java/JavaConcepts/Multithreading/ThreadSynchronization/SynchronisedMethod.java
@@ -1,21 +1,17 @@
 package JavaConcepts.Multithreading.ThreadSynchronization;
 
+import JavaConcepts.Multithreading.*;
+
 public class SynchronisedMethod {
 	public static void main(String[] args) throws InterruptedException {
 		CounterMethod c = new CounterMethod(1);
-//		Created the thread using thread class for practise.
-//		Thread created by Runnable and thread class does the same thing.
-//		Can be tried by uncommenting below code.
-//		Runnable runnable = () -> {
-//			for (int i = 0; i < 2000; i++) {
-//				c.increment();
-//			}
-//		};
-//		Thread t1 = new Thread(runnable);
-//		Thread t2 = new Thread(runnable);
-
-		MyThread t1 = new MyThread(c);
-		MyThread t2 = new MyThread(c);
+		Runnable runnable = () -> {
+			for (int i = 0; i < 2000; i++) {
+				c.increment();
+			}
+		};
+		Thread t1 = new Thread(runnable);
+		Thread t2 = new Thread(runnable);
 
 		t1.start();
 		t2.start();
@@ -24,21 +20,6 @@ public class SynchronisedMethod {
 		t2.join();
 
 		System.out.println(c.getCounter());
-	}
-}
-
-class MyThread extends Thread {
-	private CounterMethod c;
-
-	public MyThread(CounterMethod c) {
-		this.c = c;
-	}
-
-	@Override
-	public void run() {
-		for (int i = 0; i < 2000; i++) {
-			c.increment();
-		}
 	}
 }
 
