bfff4b39910d99abd584e606c3e26a7cc22f4c1d
Binay Paul <binay.paul.1993@gmail.com>
2026-01-30 16:07:38 +0530

    verbiage change

--- src/main/java/JavaConcepts/Polymorphism/Overloading/Tricky/b_EAWBV.java
+++ src/main/java/JavaConcepts/Polymorphism/Overloading/Tricky/b_EAWBV.java
@@ -11,8 +11,8 @@ When multiple types of conversions are possible, Java follows a strict "order of
 
 1.E - Exact Match
 2.A - Automatic Type Promotion (e.g., int to long)
-3.W - Widening Reference Conversion (e.g. int to Number/Object)
-4.B - Boxing/Autoboxing(e.g., int to Integer)
+3.B - Boxing/Autoboxing(e.g., int to Integer)
+4.W - Widening Reference Conversion (e.g. int to Number/Object)
 5.V - Varargs (e.g., int...)
  */
 
