782ded3d3389f9ad21123f81ac4c2c0b9521bc2b
Binay Paul <binay.paul.1993@gmail.com>
2026-02-17 22:24:43 +0530

    verbiage change

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,11 +4,7 @@ import java.util.stream.*;
 public class Test {
 
     public static void main(String[] args) {
-        List<String> passwords = List.of("$$$$$$$$$");
-        String str = passwords.stream()
-                .filter(password->password.matches("^(?=.*[a-zA-Z])(?=.*\\d).{5,}$"))
-                .collect(Collectors.joining(" "));
-        System.out.println(str);
+        System.out.println("str");
     }
 
 }
