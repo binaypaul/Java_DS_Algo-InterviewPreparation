5a0c9f1ac7e543a309e030a64ce1f4c0420eb3cf
Binay Paul <binay.paul.1993@gmail.com>
2026-03-21 00:01:14 +0530

    verbiage change

--- src/main/java/DataStructure/Neetcode150/H_Backtracking/a_Subsets_ForLoopWithSingleRecursion.java
+++ src/main/java/DataStructure/Neetcode150/H_Backtracking/a_Subsets_ForLoopWithSingleRecursion.java
@@ -16,16 +16,16 @@ public class a_Subsets_ForLoopWithSingleRecursion {
     }
 
     private void dfs(int start, int[] nums) {
+        res.add(new ArrayList<>(cur));  // Add current subset BEFORE branching
         for (int i = start; i < nums.length; i++) {
-            cur.add(nums[i]);                 //add ith elem
-            dfs(i+1, nums);             //recursive - move to next elems after ith elem
-            cur.remove(cur.size()-1);  //backtrack - remove the last elem from cur
+            cur.add(nums[i]);       // Add ith elem
+            dfs(i + 1, nums); // Recurse to next elements after ith
+            cur.removeLast();       // Backtrack - remove the last elem from cur
         }
-
-        res.add(new ArrayList<>(cur));  //once i crossed last num, add cur to res
     }
 
     public static void main(String[] args) {
         System.out.println(new a_Subsets_ForLoopWithSingleRecursion().subsets(new int[] {1,2,3}));
+        //O/P: [[], [1], [1, 2], [1, 2, 3], [1, 3], [2], [2, 3], [3]]
     }
 }
