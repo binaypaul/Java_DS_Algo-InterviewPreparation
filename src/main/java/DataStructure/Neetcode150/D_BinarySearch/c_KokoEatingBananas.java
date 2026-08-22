c15dc1ca954c7c16af59526d525abc6b306af80c
Binay Paul <binay.paul.1993@gmail.com>
2026-08-23 01:38:02 +0530

    verbiage change

--- src/main/java/DataStructure/Neetcode150/D_BinarySearch/c_KokoEatingBananas.java
+++ src/main/java/DataStructure/Neetcode150/D_BinarySearch/c_KokoEatingBananas.java
@@ -5,32 +5,42 @@ import java.util.Arrays;
 public class c_KokoEatingBananas {
     public static void main(String[] args) {
         System.out.println(minEatingSpeed(new int[] {3,6,7,11}, 8));
+//        System.out.println(minEatingSpeed(new int[]{805306368,805306368,805306368},1000000000));
     }
 
     public static int minEatingSpeed(int[] piles, int h) {
         Arrays.sort(piles);
-        int low = 0, high = piles[piles.length-1];
-        int minimum = high;
+        int l=1,r=piles[piles.length-1];
+        int min=r;
 
-        while (low <= high) {
-            int mid = (low+high)/2;
-            double count = eatBanana(piles, mid);
-            if(count > h) {
-                low = mid + 1;
+        while (l<=r) {
+            int mid = l+(r-l)/2;
+            long requiredHours = eatBanana(piles, mid);
+            if(requiredHours <= h) {
+                min = Math.min(min, mid);
+                r = mid-1;
             } else {
-                minimum = Math.min(mid, minimum);
-                high = mid -1;
+                l = mid+1;
             }
         }
-        return minimum;
+        return min;
     }
-
-    public static double eatBanana(int[] piles, int mid) {
-        double count = 0.0;
-        for (int bananasAtIndex : piles) {
-            count = count + Math.ceil(((double) bananasAtIndex/(double) mid));
+    //koko is eating 'mid' numbers of banana every hour.
+    private static long eatBanana(int[] piles, int mid) {
+        // long is used because if each pile is Integer.MAX_VALUE and h=Integer.MAX_VALUE and mid=1; then required hours is sum of each pile which crosses Integer.MAX_VALUE adn so long is required. (eg: test case 2)
+        long requiredHours=0;
+        for (int pile : piles) {
+            if(pile<=mid) {
+                // koko will just need 1 hour to eat all banana in that pile.
+                requiredHours+=1;
+            } else {
+                // koko will need 'pile/mid' hour to eat most bananas, plus 1 hour to eat remaining banana in that pile.
+                // eg: koko eating at 3 bananas every hour and a pile has 8 bananas;
+                // so koko will need 'pile/mid'=2 hour,
+                // plus 1 more hour to eat the remailing 2 bananas if there are remaining bananas.
+                requiredHours+=(pile%mid==0)?(pile/mid):pile/mid+1;
+            }
         }
-        return count;
+        return requiredHours;
     }
-
 }
--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,14 +1,43 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Test {
     public static void main(String[] args) {
-        // System.out.println(Arrays.toString(new Test().threeSum(new int[] { -1, 0, 1, 2, -1, -4 }, 2)));
-        System.out.println(new Test().trap(new int[]    {4,2,0,3,2,5}));
-                                                    //   0,1,2,3,4,5,6,7,8,9,10,11
-    }
+        System.out.println(new Test().minEatingSpeed(new int[]{805306368,805306368,805306368},1000000000));
+    }                                                         //0, 1,2 ,3, 4
 
-    public int trap(int[] height) {
-        return 0;
+
+    public int minEatingSpeed(int[] piles, int h) {
+        Arrays.sort(piles);
+        int l=1,r=piles[piles.length-1];
+        int min=r;
+
+        while (l<=r) {
+            int mid = l+(r-l)/2;
+            long requiredHours = eatBanana(piles, mid);
+            if(requiredHours <= h) {
+                min = Math.min(min, mid);
+                r = mid-1;
+            } else {
+                l = mid+1;
+            }
+        }
+        return min;
+    }
+    //koko is eating 'mid' numbers of banana every hour.
+    private long eatBanana(int[] piles, int mid) {
+        long requiredHours=0;
+        for (int pile : piles) {
+            if(pile<=mid) {
+                // koko will just need 1 hour to eat all banana in that pile.
+                requiredHours+=1;
+            } else {
+                // koko will need 'pile/mid' hour to eat most bananas, plus 1 hour to eat remaining banana in that pile.
+                // eg: koko eating at 3 bananas every hour and a pile has 8 bananas;
+                // so koko will need 'pile/mid'=2 hour,
+                // plus 1 more hour to eat the remailing 2 bananas if there are remaining bananas.
+                requiredHours+=(pile%mid==0)?(pile/mid):pile/mid+1;
+            }
+        }
+        return requiredHours;
     }
 }
